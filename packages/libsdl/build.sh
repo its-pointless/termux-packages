df08be8ba20af7a89f57b24d8c72100c9c2c0580
its-pointless <bored@goat.si>
2018-02-21 17:19:31 +1100

    asdf

--- packages/libsdl/build.sh
+++ packages/libsdl/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.libsdl.org
 TERMUX_PKG_DESCRIPTION="Simple DirectMedia Layer is a cross-platform development library "
 TERMUX_PKG_VERSION=1.2.15
-TERMUX_PKG_SRCURL=https://www.libsdl.org/release/SDL-1.2.15.tar.gz
+TERMUX_PKG_SRCURL=https://www.libsdl.org/release/SDL-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=d6d316a793e5e348155f0dd93b979798933fb98aa1edebcc108829d6474aad00
-TERMUX_PKG_DEPENDS="xorg-server"
+TERMUX_PKG_DEPENDS="xorg-server, libpulseaudio, libx11, libdbus, libxcb, libflac, libxau, libsm, libuuid"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-x"
