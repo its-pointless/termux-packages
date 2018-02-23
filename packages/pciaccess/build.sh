a3c18b8a2ba7354569301c0ffd1e268aa8b8d83b
its-pointless <bored@goat.si>
2018-02-23 13:36:37 +1100

    rm cruft

--- packages/pciaccess/build.sh
+++
@@ -1,8 +0,0 @@
-TERMUX_PKG_HOMEPAGE=https://ohse.de/uwe/software/lrzsz.html
-TERMUX_PKG_DESCRIPTION="Tools for zmodem/xmodem/ymodem file transfer"
-TERMUX_PKG_VERSION=0.12.20
-TERMUX_PKG_SRCURL=https://www.x.org/archive//individual/lib/libpciaccess-0.14.tar.gz
-TERMUX_PKG_SHA256=8d86e64893917be3dfb1c5e837888d1275399c818783474002203d751312b03c
-#TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=""
-#TERMUX_PKG_HOSTBUILD=true
