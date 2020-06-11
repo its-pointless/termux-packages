06d8daf2e273a9453aca3b365f30a5b4dee101c3
Noah <33094578+coolreader18@users.noreply.github.com>
2020-06-10 21:16:15 -0500

    rust: update to 1.44.0

--- packages/rust/build.sh
+++ packages/rust/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.rust-lang.org/
 TERMUX_PKG_DESCRIPTION="Systems programming language focused on safety, speed and concurrency"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Kevin Cotugno @kcotugno"
-TERMUX_PKG_VERSION=1.43.1
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=1.44.0
 TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/rustc-$TERMUX_PKG_VERSION-src.tar.xz
-TERMUX_PKG_SHA256=eb0a103c67c4565403d9e6f84a1c708982a5e9e5b3c0d831e4d6f6451795d106
+TERMUX_PKG_SHA256=b32fa7d6bd6ff9286aab2aa7ca696d1da921614cefc10f617aef68d9bce9118a
 TERMUX_PKG_DEPENDS="libc++, clang, openssl, lld, zlib, libllvm"
 termux_step_configure() {
 	termux_setup_cmake
@@ -18,7 +17,7 @@ termux_step_configure() {
 	# like 30 to 40 + minutes ... so lets get it right
 
 	# upstream only tests build ver one version behind $TERMUX_PKG_VERSION
-	local BOOTSTRAP_VERSION=1.42.0
+	local BOOTSTRAP_VERSION=1.43.1
 	rustup install $BOOTSTRAP_VERSION
 	rustup default $BOOTSTRAP_VERSION-x86_64-unknown-linux-gnu
 	export PATH=$HOME/.rustup/toolchains/$BOOTSTRAP_VERSION-x86_64-unknown-linux-gnu/bin:$PATH
